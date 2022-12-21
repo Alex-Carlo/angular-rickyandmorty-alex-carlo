04a6fc7a2be71c9f49ef211a08ee08fc3fa92ee7
Alex Carlo <alancamo2015@gmail.com>
2022-12-21 18:33:07 -0500

    Instalacion de dependecias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ name:'TEST' })
   ],
   providers: [],
   bootstrap: [AppComponent]
